2e2c4dd4abd3ecd7f06e505b56511bd3ef24707a
kirti-singla123 <kirtisingla38231@gmail.com>
2025-04-15 21:57:41 -0400

    changed Creamy.wsgi

--- Creamy/Creamy/wsgi.py
+++ Creamy/Creamy/wsgi.py
@@ -10,12 +10,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-import sys
 import os
-
-# Add the project directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'Creamy')))
-
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Creamy.settings')
+
 application = get_wsgi_application()
+
